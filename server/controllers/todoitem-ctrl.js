9b01be430c16c4b025ac04e7c5125e0be836a003
WilliamJGrace <williamjgrace@outlook.com>
2020-06-22 11:24:42 +0100

    create item ctrl created

---
+++ server/controllers/todoitem-ctrl.js
@@ -0,0 +1,46 @@
+const { model } = require("../db");
+
+const TodoItem = require(../models/todoitem-model)
+
+
+createTodoItem = (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must prove a todo item'
+        })
+    }
+
+    const todoItem = new TodoItem(body)
+
+    if (!todoItem) {
+        return res.status(400).json({
+            success: false,
+            error: err
+        })
+    }
+
+    todoItem
+        .save()
+        .then(() => {
+            return res.status(201).json({
+                success: true,
+                id: todoItem._id,
+                message: 'Todoitem created!'
+            })
+        })
+        .catch(error => {
+            return res.status(400).json({
+                error,
+                message: 'Todoitem not created!'
+            })
+        })
+        
+        
+        
+
+}
+
+
--- server/models/todoitem-model.js
+++ server/models/todoitem-model.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Todoitem = new Schema(
+const TodoItem = new Schema(
     {
         name: { type: String, required: true },
         isCompleted: { type: Boolean, default: false}
@@ -10,4 +10,4 @@ const Todoitem = new Schema(
     { timestamps: true },
 )
 
-module.exports = mongoose.model('todoitems', Todoitem)
+module.exports = mongoose.model('todoitems', TodoItem)
