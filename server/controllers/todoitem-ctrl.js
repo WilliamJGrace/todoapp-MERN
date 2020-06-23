9fb305f2b57a545fc3f11506af308e801706ad69
WilliamJGrace <williamjgrace@outlook.com>
2020-06-23 11:11:35 +0100

    Update todoitem

--- server/controllers/todoitem-ctrl.js
+++ server/controllers/todoitem-ctrl.js
@@ -1,6 +1,7 @@
 const { model } = require("../db");
 
-const TodoItem = require('../models/todoitem-model')
+const TodoItem = require('../models/todoitem-model');
+const todoitemModel = require("../models/todoitem-model");
 
 
 createTodoItem = (req, res) => {
@@ -41,6 +42,37 @@ createTodoItem = (req, res) => {
         
         
 
+}
+
+updateTodoItem = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update'
+        })
+    }
+
+    TodoItem.findOne({ _id: req.params.id }, (err, todoItem) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'TodoItem Not Found!'
+            })
+        }
+        todoItem.name = body.name
+        todoItem.isCompleted = body.isCompleted
+        todoItem
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: todoitem._id,
+                    message: 'Todoitem updated!'
+                })
+            })
+    } )
 }
 
 
