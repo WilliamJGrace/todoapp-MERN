e363a909dece0f5be5fde80449cbaaa9c529914f
WilliamJGrace <williamjgrace@outlook.com>
2020-06-27 16:19:07 +0100

    hardcoded todo items viewable

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoItems, setTodoItems] = useState(
+    [{"name": 'do the dishes'},
+    {"name": 'wash clothes'},
+    {"name": 'clean car'}
+  ]
+  )
+  
+  const TodoItem = ({ todoItem }) => <div className="todo">{todoItem.name}</div>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todoItems.map((todoItem, index) => (
+          <TodoItem
+            key={index}
+            index={index}
+            todoItem={todoItem}
+          />
+        ))}
+      </div>
     </div>
+  
   );
 }
 
