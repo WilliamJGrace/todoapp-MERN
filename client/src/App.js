ffb38862152a8f888943047451f9fd341bfd2068
WilliamJGrace <williamjgrace@outlook.com>
2020-06-29 14:54:33 +0100

    can fetch todo items from api, can make post req

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+const TodoItem = ({ todoItem }) => <div className="todo">{todoItem.name}</div>;
+
+
+
+function TodoForm({ addTodoItem }) {
+  const [value, setValue] = useState("");
+
+ 
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if(!value) return;
+    // addTodoItem(value)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: value })
+  };
+  fetch("http://localhost:3000/api/todoitem", requestOptions)
+  .then(response => response.json())
+  // .then(data => add(data.id));
+    setValue("")
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </form>
+  )
+}
 
 function App() {
   const [todoItems, setTodoItems] = useState(
-    [{"name": 'do the dishes'},
-    {"name": 'wash clothes'},
-    {"name": 'clean car'}
-  ]
+    []
   )
-  
-  const TodoItem = ({ todoItem }) => <div className="todo">{todoItem.name}</div>;
+  useEffect(() => {
+    fetch("http://localhost:3000/api/todoitems")
+      .then(res => res.json())
+      .then(data =>{
+        setTodoItems(data.data)
+      }) 
+  })
+
+  const addTodoItem = name => {
+    const newTodoItems = [...todoItems, { name }];
+    setTodoItems(newTodoItems)
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
@@ -20,6 +63,7 @@ function App() {
             todoItem={todoItem}
           />
         ))}
+        <TodoForm addTodoItem={addTodoItem} />
       </div>
     </div>
   
