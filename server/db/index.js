c56f2bf87697491d72873e2e7d58cc4131cdec66
WilliamJGrace <williamjgrace@outlook.com>
2020-07-10 14:12:43 +0100

    updated index sercver

--- server/db/index.js
+++ server/db/index.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose')
 
 mongoose
     .connect('mongodb+srv://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@cluster0.obclx.mongodb.net/todoapp?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then(() => {
+        console.log("database connected")
+    })
     .catch(e => {
         console.error('Connection error', e.message)
     })
