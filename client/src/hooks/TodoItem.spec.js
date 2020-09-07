5ba2cb826a3bca1bc4c9b64277fba851aa52fcfd
WilliamJGrace <williamjgrace@outlook.com>
2020-09-07 11:13:42 +0100

    added before each for shallow and mount

--- client/src/hooks/TodoItem.spec.js
+++ client/src/hooks/TodoItem.spec.js
@@ -1,3 +1,14 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import TodoItem from './TodoItem'
+
+describe('TodoItem', () => {
+
+    let wrapper
+    let wrapperMount
+
+    beforeEach(() => wrapper = shallow(<TodoItem />))
+
+    beforeEach(() => wrapperMount = mount(<TodoItem />))
+    
+});
