865956a4fa1ea1686cb905934efee2dc53b7d6f3
WilliamJGrace <williamjgrace@outlook.com>
2020-09-24 15:27:59 +0100

    create todolist controller

--- server/controllers/todolist-ctrl.js
+++ server/controllers/todolist-ctrl.js
@@ -1,2 +1,38 @@
+const todolistModel = require("../models/todolist-model")
 const TodoList = require("../models/todolist-model")
 
+createTodoList = (req, res) => {
+
+    const body = req.body
+
+    if(!body){
+        return res.status(400).json({
+            success: false,
+            error: 'You must prove a todo item'
+        })
+    }
+
+    const todoList = new TodoList(body)
+
+    todoList
+    .save()
+    .then(() => {
+        return res.status(201).json({
+            success: true,
+            id: todoList._id,
+            message: 'Todolist created!'
+        })
+
+    }
+    )
+
+
+
+
+}
+
+module.exports = {
+    createTodoList
+   
+
+}
