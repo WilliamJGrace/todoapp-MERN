6ed33ce037865bd84abd4d8f49277e99a98397b9
WilliamJGrace <williamjgrace@outlook.com>
2020-09-24 17:02:30 +0100

    Todolistform init

---
+++ client/src/hooks/TodoListForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function TodoListForm({ createTodoList }) {
+    const [value, setValue] = useState("");
+  
+   
+  
+    const handleSubmit = event => {
+      event.preventDefault();
+      if(!value) return;
+      createTodoList(value)
+      setValue("")
+    }
+    return (
+        <>
+        <p>New List:</p>
+      <form onSubmit={handleSubmit}>
+        <input id="nametodo"
+        data-test="inputElement"
+          type="text"
+          className="input"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+        />
+      </form>
+      </>
+    )
+  }
+
+export default TodoListForm
